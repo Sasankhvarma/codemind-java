efa480c4d45d6c57aabc9cae08a806bc99369ba5
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-04-28 22:59:22 +0530

    import java.util.Scanner;
    class jagan
    {
        public static void main(String args[])
        {
            int n,i;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            int []arr=new int[n];
            for(i=0;i<n;i++)
                arr[i]=sc.nextInt();
            if(n%2==0)
            {
                for(i=0;i<n/2;i++)
                {
                    System.out.printf("%d %d ",arr[i],arr[n-i-1]);
                }
            }
            else
            {
                for(i=0;i<n/2;i++)
                {
                    System.out.printf("%d %d ",arr[i],arr[n-i-1]);
                }
                System.out.printf("%d 0",arr[(n/2)]);
            }
        }
    }

---
+++ First_and_Last_pair.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class jagan
+{
+    public static void main(String args[])
+    {
+        int n,i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []arr=new int[n];
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        if(n%2==0)
+        {
+            for(i=0;i<n/2;i++)
+            {
+                System.out.printf("%d %d ",arr[i],arr[n-i-1]);
+            }
+        }
+        else
+        {
+            for(i=0;i<n/2;i++)
+            {
+                System.out.printf("%d %d ",arr[i],arr[n-i-1]);
+            }
+            System.out.printf("%d 0",arr[(n/2)]);
+        }
+    }
+}
