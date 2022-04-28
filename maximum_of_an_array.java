10b4b1ed0cd355e380701e8fa29c9f1829ca4a50
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-04-29 00:15:50 +0530

    import java.util.Scanner;
    class jagan
    {
        public static void main(String args[])
        {
            int n,i,j;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            int []arr=new int[n];
            for(i=0;i<n;i++)
                arr[i]=sc.nextInt();
            int c=0,s=0,rep=-1;
            for(i=0;i<n;i++)
            {
               if(arr[i]>rep)
               {
                   rep=arr[i];
               }
            }
            System.out.printf("%d",rep);
        }
    }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class jagan
+{
+    public static void main(String args[])
+    {
+        int n,i,j;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []arr=new int[n];
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int c=0,s=0,rep=-1;
+        for(i=0;i<n;i++)
+        {
+           if(arr[i]>rep)
+           {
+               rep=arr[i];
+           }
+        }
+        System.out.printf("%d",rep);
+    }
+}
