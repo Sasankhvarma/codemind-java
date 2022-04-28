ecd1c8be8b4adabf241bdff06f3713bf6b427d97
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-04-29 00:13:10 +0530

    import java.util.Scanner;
    class jagan
    {
        public static void main(String args[])
        {
            int n,i,j;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            int []arr=new int[n];
            for(i=0;i<n;i++)
                arr[i]=sc.nextInt();
            int c=0,s=0,rep;
            for(i=0;i<n;i++)
            {
               s=s+arr[i];
            }
            float avg=(float)s/n;
            System.out.printf("%.2f",avg);
        }
    }

---
+++ average_of_the_array.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class jagan
+{
+    public static void main(String args[])
+    {
+        int n,i,j;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []arr=new int[n];
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int c=0,s=0,rep;
+        for(i=0;i<n;i++)
+        {
+           s=s+arr[i];
+        }
+        float avg=(float)s/n;
+        System.out.printf("%.2f",avg);
+    }
+}
