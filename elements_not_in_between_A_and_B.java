c3fba8f2058424dcad36fc76958f6e05aeb24212
Sasankhvarma <96321069+Sasankhvarma@users.noreply.github.com>
2022-04-28 23:41:05 +0530

    import java.util.Scanner;
    class jagan
    {
        public static void main(String args[])
        {
            int n,i,j;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            int []arr=new int[n];
            for(i=0;i<n;i++)
                arr[i]=sc.nextInt();
            int c,s=0,rep,a,b;
            a=sc.nextInt();
            b=sc.nextInt();
            for(i=0;i<n;i++)
            {
                if(arr[i]<a || arr[i]>b)
                {
                    System.out.printf("%d ",arr[i]);
                    s=1;
                }
            }
            if(s==0)
            {
                System.out.printf("-1");
            }
        }
    }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class jagan
+{
+    public static void main(String args[])
+    {
+        int n,i,j;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int []arr=new int[n];
+        for(i=0;i<n;i++)
+            arr[i]=sc.nextInt();
+        int c,s=0,rep,a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=0;i<n;i++)
+        {
+            if(arr[i]<a || arr[i]>b)
+            {
+                System.out.printf("%d ",arr[i]);
+                s=1;
+            }
+        }
+        if(s==0)
+        {
+            System.out.printf("-1");
+        }
+    }
+}
